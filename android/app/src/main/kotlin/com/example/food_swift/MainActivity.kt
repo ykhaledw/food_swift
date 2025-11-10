4ec21a3ccbb9554dc70cd604d6351922d80aa5cc
youssefkhaled <youssefwahdan12@gmail.com>
2025-11-10 19:26:38 +0200

    Project Created

---
+++ android/app/src/main/kotlin/com/example/food_swift/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.food_swift
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:food_swift/food_swift.dart';
+
+void main() {
+  runApp(const FoodSwift());
+}
+
