7cb0365fc5662f5dbcb728f05bf54de9dd0cddda
youssefkhaled <youssefwahdan12@gmail.com>
2025-11-11 21:57:36 +0200

    Second setup: Added font weight helper file added text styles

---
+++ lib/core/theming/text_styles.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:food_swift/core/theming/font_weight_helper.dart';
+
+class TextStyles {
+  static TextStyle font32BlackBold = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font28BlackBold = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeightHelper.bold,
+    color: Colors.black,
+  );
+  static TextStyle font24BlackBold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font22BlackBold = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font18BlackBold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeightHelper.bold,
+    color: Colors.black,
+  );
+  static TextStyle font16BlackBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font16BlackRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeightHelper.regular,
+  );
+  static TextStyle font16BlackMedium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle font14BlackBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font14BlackRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeightHelper.regular,
+  );
+  static TextStyle font14BlackMedium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle font12BlackMedium = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeightHelper.medium,
+  );
+}
